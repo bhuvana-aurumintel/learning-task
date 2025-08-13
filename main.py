406afe770cc656bcf3163dd11b34d8ac4f726c57
bhuvana-aurumintel <bhuvana@aurumintel.com>
2025-08-13 18:14:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,8 @@ from fastapi import FastAPI
 app = FastAPI()
 @app.get("/")
 def home():
+    return {"message":"this is learning task"}
+@app.get("/health")
+def health_check():
     return {"health is good"}
 
